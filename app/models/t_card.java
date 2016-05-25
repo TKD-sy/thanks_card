648c26a02967b0dc916f03d8e9e2590d2c4404f7
SPL-023 <SPL-023@SPL-023-PC>
2016-05-25 15:32:41 +0900

    aaa

---
+++ app/models/t_card.java
@@ -0,0 +1,26 @@
+package models;
+
+import com.avaje.ebean.Model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class t_card extends Model {
+	@Id
+    public Integer card_id;
+    public Integer category_id;
+    public Integer sousin_id;
+    public Integer jyusin_id;
+    public Integer hensin_id;
+    public Integer card_kidokuflag;
+    public Integer card_flag;
+    public Integer card_hensinflag;
+    public String card_help;
+    public String card_comment;
+    public Date card_date;
+
+
+    public static Find<Integer,t_card> find = new Find<Integer,t_card>(){};
+}
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table t_card (
+  card_id                       integer not null,
+  category_id                   integer,
+  sousin_id                     integer,
+  jyusin_id                     integer,
+  hensin_id                     integer,
+  card_kidokuflag               integer,
+  card_flag                     integer,
+  card_hensinflag               integer,
+  card_help                     varchar(255),
+  card_comment                  varchar(255),
+  card_date                     timestamp,
+  constraint pk_t_card primary key (card_id)
+);
+create sequence t_card_seq;
+
+
+# --- !Downs
+
+drop table if exists t_card;
+drop sequence if exists t_card_seq;
+

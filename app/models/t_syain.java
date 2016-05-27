ffb2ccb5eee72446203143ecec5d63821303956e
SPL-029 <SPL-029@SPL-029-PC>
2016-05-27 13:05:50 +0900

    消えたブランチの復元

--- app/models/t_syain.java
+++ app/models/t_syain.java
@@ -31,8 +31,21 @@ public class t_syain extends Model {
 	    public String syain_sex;
 
 	    public static Find<Integer,t_syain> find = new Find<Integer,t_syain>(){};
-	    
-	    
+
+
+//	    authenticateの中をみてfalseだったらエラー文をhasErrorに入れる。
+	    public String validate() {
+	        if (authenticate(syain_id, syain_pass)) {
+	            return null;
+	        }
+	        return "※社員IDかパスワードが間違っています";
+	    }
+
+//	    syain_Idとsyain_Passの中身がDBの中にあるかどうかを判断する
+	    public static Boolean authenticate(Integer Id, String Pass) {
+	        t_syain user = t_syain.find.where().eq("syain_id", Id).findUnique();
+	        return (user != null && user.syain_pass.equals(Pass));
+	    }
 
 
 }

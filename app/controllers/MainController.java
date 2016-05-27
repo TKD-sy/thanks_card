7a7e7d033b4ec797a3cebea5b2614f1e9ac1d27a
SPL-023 <SPL-023@SPL-023-PC>
2016-05-27 13:59:21 +0900

    事例選択ができるバージョン

--- app/controllers/MainController.java
+++ app/controllers/MainController.java
@@ -19,3 +19,9 @@ public class MainController  extends Controller{
 
 
 
+//    mypage.scala.htmlの人事、経営用のボタンコード
+/* 			@if(User.authenticate(役職)="人事" || User.authenticate(役職)="経営"){
+	<button type="button" class="btn btn-primary btn-lg myp_links">追加</button><br>
+	<button type="button" class="btn btn-primary btn-lg myp_links">編集</button><br>
+	<button type="button" class="btn btn-primary btn-lg myp_links">代表事例選択</button><br>
+	}*/
